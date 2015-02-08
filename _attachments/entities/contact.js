debfd96357803990fcd4ced1c297fc063fadb5c8
ATonkovid <assembly26@gmail.com>
2015-02-08 19:40:35 +0200

    Added contact entity

---
+++ _attachments/entities/contact.js
@@ -0,0 +1,11 @@
+﻿define(['angular', 'CouchEntity'], function (angular, CouchEntity) {
+    angular.module('fiveOClock').factory('Contact', function (CouchEntity) {
+        var entity = {
+            type: 'contact',
+            props: ['name', 'email', 'phone'],
+            url: '_view/contact'
+        };
+
+        return new CouchEntity(entity);
+    });
+});
---
+++ views/contact/map.js
@@ -0,0 +1,5 @@
+﻿function(doc){
+    if(doc.type == 'contact'){
+        emit(doc._id, doc);
+    }
+}
