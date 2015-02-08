cb0fd1b3cdfea6839d1ecc00a068a8e085a09e66
ATonkovid <assembly26@gmail.com>
2015-02-08 19:09:10 +0200

    Initialized project structure

---
+++ _attachments/main.js
@@ -0,0 +1,27 @@
+﻿requirejs.config({
+    paths: {
+        'angular': 'lib/angular',
+        'underscore': 'lib/underscore',
+        'jquery': 'lib/jquery-2.1.1',
+        'bootstrap': 'lib/bootstrap/js/bootstrap',
+        'CouchEntity': 'CouchOrm/CouchEntity',
+        'CouchEntityFactory': 'CouchOrm/CouchEntityFactory'
+    },
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['jquery', 'bootstrap', 'angular', 'CouchEntity'], function ($, bootstrap, angular) {
+    angular.module('fiveOClock', ['angularCouch']);
+    $(function () {
+        require(['app/app'], function () {
+            angular.bootstrap('body', ['fiveOClock']);
+        });
+    });
+});
