25f771d0e4aa77098629a95d66eaccdd4c5923ed
ATonkovid <assembly26@gmail.com>
2015-02-10 09:36:30 +0200

    10.02.2015(Add/dell/red)

--- _attachments/app/app.js
+++ _attachments/app/app.js
@@ -1,8 +1,47 @@
 ﻿define(['angular', 'entities/contact'], function (angular, contact) {
-    angular.module('fiveOClock').controller('AppController', function ($scope, Contact) {
-        $scope.message = 'Hello world';
+    angular.module('fiveOClock').controller('AppController', function ($scope, Contact) {       
         Contact.get().then(function (contacts) {
-            console.log(contacts);
-        });
+            $scope.AllContacts = contacts;
+        });       
+        $scope.isLabel = function (ElementContact) {       
+            return !ElementContact.typeLabel;
+        }
+        $scope.RedactEl = function (ElementContact) {             
+            var that = this;
+            that.indexEl = that.AllContacts.indexOf(ElementContact);
+            Contact.delete(ElementContact._id, ElementContact._rev).then(function () {
+                var index = that.AllContacts.indexOf(ElementContact);
+                that.AllContacts.splice(index, 1);
+            });
+            Contact.post({ name: ElementContact.name, email: ElementContact.email, phone: ElementContact.phone }).then(function (data) {
+                that.AllContacts.splice(that.indexEl, 0, data);
+            });
+        }
+        $scope.AddItem = function () {
+            var that = this;
+            Contact.post({ name: this.NewNameNg }).then(function (data) {
+                that.AllContacts.splice(that.indexEl, 0, data);
+                that.NewNameNg = "";
+            });
+        };
+        $scope.removeTask = function (ElementContact) {
+            var that = this;
+            Contact.delete(ElementContact._id, ElementContact._rev).then(function () {
+                var index = that.AllContacts.indexOf(ElementContact);
+                that.AllContacts.splice(index, 1);
+            });
+        };
+        $scope.SaveDate = function (ElementContact) {           
+            var that = this;
+            that.indexEl = that.AllContacts.indexOf(ElementContact);
+            Contact.delete(ElementContact._id, ElementContact._rev).then(function () {
+                var index = that.AllContacts.indexOf(ElementContact);
+                that.AllContacts.splice(index, 1);
+            });
+            Contact.post({ name: ElementContact.name, email: ElementContact.email, phone: ElementContact.phone }).then(function (data) {
+                that.AllContacts.splice(that.indexEl, 0, data);
+            });
+        }
+
     });
 });
